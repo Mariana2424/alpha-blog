9a4ebe2c4670467a960f8f001660c0d15fb05a1d
Mariana <marianagogashvili24@gmail.com>
2017-08-16 10:24:50 +0300

    Style

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
      if @article.save
-        flash[:notice] = "Article was created"
+        flash[:success] = "Article was created"
         redirect_to article_path(@article)
      else
         render 'new'
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
   def update
     
     if @article.update(article_params)
-      flash[:notice] = "Artcile was updated"
+      flash[:success] = "Artcile was updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
   def destroy
     
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
+    flash[:success] = "Article was successfully deleted"
     redirect_to articles_path
   end
   private
